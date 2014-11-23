bb6c4c530f5cbf35b800d54fac4c3dd5933539d9
Zia Siddique <siddiquezia@hotmail.com>
2014-11-23 15:12:36 +0000

    Created FlightController and InputController
    Mostly completed the new scripts.
    Also modified the camera script

---
+++ HorizonUnity/Assets/HorizonScripts/FlightController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+public class FlightController : MonoBehaviour {
+
+	// Public Variables
+	public float maxThrust;
+	public float maxAngularVelocity;
+
+	// Private Variables
+	float _thrust; float thrust {
+		get { 
+			return _thrust; 
+		}
+		set { 
+			_thrust = value;
+			if (_thrust > maxThrust) _thrust = maxThrust;
+			if (_thrust < 0) _thrust = 0;
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+		thrust = 0;
+		rigidbody.maxAngularVelocity = maxAngularVelocity;
+	}
+
+	// Physics update
+	void FixedUpdate () {
+
+		// Apply thrust
+		this.rigidbody.AddRelativeForce (new Vector3(0,thrust,0));
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		SetThrustPercentage (100);
+	}
+
+	// Increase Thrust
+
+	// Set thrust to a percentage of ship's maximum thrust
+	public void SetThrustPercentage (float percentage)
+	{
+		if (percentage == 0) 
+			thrust = 0;
+		else 
+			thrust = maxThrust * (percentage / 100);
+	}
+
+	// Add a specific value to the thrust
+	public void AddThrust (float amount)
+	{
+		thrust += amount;
+	}
+
+	// Set thrust to to a specific value
+	public void SetThrust (float amount)
+	{
+		thrust = amount;
+	}
+
+	// Pitch rotation
+	public void Pitch (float amount) {
+		this.rigidbody.AddRelativeTorque (new Vector3 (amount,0,0));
+	}
+
+	// Yaw rotation
+	public void Yaw (float amount) {
+		this.rigidbody.AddRelativeTorque (new Vector3 (0,0,amount));
+	}
+
+	// Roll	rotation
+	public void Roll (float amount) {
+		this.rigidbody.AddRelativeTorque (new Vector3 (0,amount,0));
+	}
+}
---
+++ HorizonUnity/Assets/HorizonScripts/InputController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (FlightController))]
+public class InputController : MonoBehaviour {
+
+	public FlightController flightController;
+
+
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void FixedUpdate()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Roll input
+		if (Input.GetAxis ("Roll") > 0) {
+				flightController.Roll (-8);
+		}
+		if (Input.GetAxis ("Roll") < 0) {
+				flightController.Roll (8);
+		}
+
+		// Yaw Input
+		float mouseXPercentage = (Input.mousePosition.x - (Screen.width / 2)) / Screen.width;
+		flightController.Yaw (mouseXPercentage * flightController.maxAngularVelocity);
+		
+		// Pitch Input
+		float mouseYPercentage = (Input.mousePosition.y - (Screen.height / 2)) / Screen.height;
+		flightController.Pitch (mouseYPercentage * flightController.maxAngularVelocity);
+	}
+}
