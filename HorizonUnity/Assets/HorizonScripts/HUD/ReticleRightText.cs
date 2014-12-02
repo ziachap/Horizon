6e171ff4f1dc28b6a93d107e53e74faccf2f26de
Zia Siddique <siddiquezia@hotmail.com>
2014-12-02 13:42:46 +0000

    Added reticle text

---
+++ HorizonUnity/Assets/HorizonScripts/HUD/ReticleRightText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReticleRightText : MonoBehaviour {
+
+	public ShipController ship;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.guiText.text = "V // " + (int)ship.flight.GetThrust() + " // " + (int)ship.flight.maxThrust;
+	}
+}
--- HorizonUnity/Assets/HorizonScripts/ThirdPersonShipCamera.cs
+++ HorizonUnity/Assets/HorizonScripts/ThirdPersonShipCamera.cs
@@ -12,10 +12,22 @@ public class ThirdPersonShipCamera : MonoBehaviour {
 	}
 	
 	void FixedUpdate () {
+
+
+
 		// Rotation
 		transform.rotation = Quaternion.Slerp(transform.rotation, 
-		                     Quaternion.LookRotation (target.up, Vector3.Cross (target.right, target.up)),
+		                    Quaternion.LookRotation (target.up, Vector3.Cross (target.right, target.up)),
 		                     0.3f);
+
+		//Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		//Vector3 point = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 1000));
+		//Vector3 dir = point - target.position;
+
+		//transform.rotation = Quaternion.Slerp(transform.rotation, 
+		//                                      Quaternion.LookRotation (dir, Vector3.Cross (target.right, target.up)),
+		//                                      0.9f);
+
 		// Position
 		Vector3 targetPos = target.position + (target.up * -cameraDistance) 
 			+ (Vector3.Cross(target.transform.up, target.right) * -cameraHeight);
